af0355295a1ca11a1b3d3567284f80c047c651ed
Leon <108039973+Leonic16246@users.noreply.github.com>
2026-03-25 00:24:39 +1300

    Add status bar

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import React from 'react';
 import { useColorScheme } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
@@ -9,6 +10,8 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar />
+      
       <AnimatedSplashOverlay />
       <AppTabs />
     </ThemeProvider>
