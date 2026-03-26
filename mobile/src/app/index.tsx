ee480bdb80034cdd1299496ab55ec9a2452274d8
Leon <108039973+Leonic16246@users.noreply.github.com>
2026-03-26 23:19:51 +1300

    Add header and logo

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react'
-
+import Logo from '../../assets/images/CherryTreeGlyph.png';
 
 export default function HomeScreen() {
   return (
-     <SafeAreaView>
-       <Text>Home</Text>
-     </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.header}>
+        <Text style={styles.title}>MyUniClubDemo</Text>
+        <Image source={Logo} style={styles.image}/>
+      </View>
+
+      <Text style={styles.greeting}>Hello, Leon</Text>
+      <Text style={styles.heading}>Recent Events</Text>
+      <Text style={styles.heading}>Latest Posts</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 8
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingVertical: 8,
+  },
+  image: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24
+  },
+  greeting: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  heading: {
+    fontSize: 16
+  }
+})
