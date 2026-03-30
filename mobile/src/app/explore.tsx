07a89542fb27702a7ca20d86c438a2f12b9f7467
Leon <108039973+Leonic16246@users.noreply.github.com>
2026-03-30 23:35:28 +1300

    Update explore.tsx

--- mobile/src/app/explore.tsx
+++ mobile/src/app/explore.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react'
 
 export default function Explore() {
+
   return (
-    <SafeAreaView>
-      <Text>explore</Text>
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.header}>
+        <Text>Buddies</Text>
+        <Text>Events</Text>
+        <Text>Marketplace</Text>
+      </View>
+
+      <View style={styles.content}>
+
+      </View>
+
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
 
-const styles = StyleSheet.create({})
+  container: {
+    padding: 8,
+    flex: 1,
+  },
+
+  header: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  width: '100%',
+  paddingVertical: 8,
+  },
+
+  content: {
+
+  }
+
+})
